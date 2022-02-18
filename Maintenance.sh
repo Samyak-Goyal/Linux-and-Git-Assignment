8698dc5dde24d91dd0907534b28d8d05f18c4241
samyak goyal <samyak.goyal@innovaccer.com>
2022-02-18 15:31:14 +0530

    added time

--- Maintenance.sh
+++ Maintenance.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo -e "\n$(date "+%T") --- Started\n"
 apt-get update
 apt-get -y upgrade
 
 apt-get -y autoremove
 apt-get autoclean
 
-echo "Done"
+echo -e "\n$(date "+%T") --- Finished"
